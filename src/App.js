f7653ecbc84e98e3d0e57e6cd62d92cb49a1c12a
NKwatra <kwatranishkarsh@gmail.com>
2019-11-12 01:51:38 +0530

    Add bootstrap and css for styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Filter from "./Filter";
+import Cocktails from "./Cocktails";
 
 function App() {
+  const [cocktails, updateCocktails] = useState([]);
+  console.log(cocktails);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+      <Filter updater={updateCocktails} />
+      <Cocktails cocktails={cocktails} />
     </div>
   );
 }
-
 export default App;
